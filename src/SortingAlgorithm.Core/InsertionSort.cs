f7efb16183ae733e9de145569acf735987a2b487
Yang, Edward <yrz1994@gmail.com>
2021-03-22 15:29:43 +0800

    优化插入排序算法。

--- src/SortingAlgorithm.Core/InsertionSort.cs
+++ src/SortingAlgorithm.Core/InsertionSort.cs
@@ -5,27 +5,21 @@ namespace SortingAlgorithm.Core
 {
     public class InsertionSort : IArraySort
     {
+        /* 插入排序
+         * 时间复杂度O(N^2)，空间复杂度：O(1)，稳定排序
+         * 最优情况：当前数组为正序数组，时间复杂度：O(N)
+         * 最差情况：当前数组为逆序数组，时间复杂度：O(N^2)
+         */
         public int[] Sort(int[] source)
         {
             if (source == null) throw new ArgumentNullException();
-            for (var i = 0; i < source.Length - 1; i++)
+            for (var i = 1; i < source.Length; i++)
             {
-                var current = source[i + 1];
-                for (var j = i; j >= 0; j--)
+                for (int j = i; j > 0 && source[j] < source[j - 1]; j--)
                 {
-                    if (current < source[j])
-                    {
-                        source[j + 1] = source[j];
-                        if(j == 0)
-                        {
-                            source[j] = current;
-                        }
-                    }
-                    else
-                    {
-                        source[j + 1] = current;
-                        break;
-                    }
+                    source[j - 1] = source[j] + source[j - 1];
+                    source[j] = source[j - 1] - source[j];
+                    source[j - 1] = source[j - 1] - source[j];
                 }
             }
             return source;

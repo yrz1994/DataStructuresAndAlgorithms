26e947622f6f78f816457ead5401b5f2d8e9fae9
Yang, Edward <yrz1994@gmail.com>
2021-04-01 16:35:35 +0800

    新增最长公共子串方法

---
+++ src/LeetCode.Core/LongestCommonString.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace LeetCode.Core
+{
+    public class LongestCommonString
+    {
+        /// <summary>
+        /// 最长公共子串
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int LongestCommonSubstringLength(string a, string b)
+        {
+            if (string.IsNullOrWhiteSpace(a) || string.IsNullOrWhiteSpace(b)) return 0;
+            var arrayA = a.ToCharArray();
+            var arrayB = b.ToCharArray();
+            int[,] table = new int[a.Length, b.Length];
+            var sum = 0;
+            for (int j = 0; j < arrayB.Length; j++)
+            {
+                for (int i = 0; i < arrayA.Length; i++)
+                {
+                    if (arrayA[i] == arrayB[j])
+                    {
+                        table[i, j] = (i == 0 || j == 0) ? 1 : table[i - 1, j - 1] + 1;
+                        sum = Math.Max(sum, table[i, j]);
+                    }
+                }
+            }
+            return sum;
+        }
+
+        /// <summary>
+        /// 最长公共子序列
+        /// </summary>
+        /// <param name="text1"></param>
+        /// <param name="text2"></param>
+        /// <returns></returns>
+        public int LongestCommonSubsequence(string text1, string text2)
+        {
+            if (string.IsNullOrWhiteSpace(text1) || string.IsNullOrWhiteSpace(text2)) return 0;
+            int[,] table = new int[text1.Length + 1, text2.Length + 1];
+            var sum = 0;
+            for (int j = 1; j <= text2.Length; j++)
+            {
+                for (int i = 1; i <= text1.Length; i++)
+                {
+                    if (text1[i-1] == text2[j-1])
+                    {
+                        table[i, j] = table[i - 1, j - 1] + 1;
+                        sum = Math.Max(sum, table[i, j]);
+                    }
+                    else
+                    {
+                        table[i, j] = Math.Max(table[i - 1, j], table[i, j - 1]);
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ src/LeetCode.UnitTest/LongestCommonStringTest.cs
@@ -0,0 +1,53 @@
+﻿using LeetCode.Core;
+using System.Collections.Generic;
+using Xunit;
+
+namespace LeetCode.UnitTest
+{
+    public class LongestCommonStringTest
+    {
+        [Theory]
+        [MemberData(nameof(LongestCommonSubstringData.TestData), MemberType = typeof(LongestCommonSubstringData))]
+        public void LongestCommonSubstringLengthTest(string a, string b, int expect)
+        {
+            //Arrange
+            var sut = new LongestCommonString(); //sut: system under test
+
+            //Act
+            var result = sut.LongestCommonSubstringLength(a, b);
+
+            //Assert
+            Assert.Equal(expect, result);
+        }
+
+        [Fact]
+        public void LongestCommonSubsequenceTest()
+        {
+            //Arrange
+            var sut = new LongestCommonString(); //sut: system under test
+
+            //Act
+            var result = sut.LongestCommonSubsequence("abcde", "ace");
+
+            //Assert
+            Assert.Equal(3, result);
+        }
+    }
+
+    public class LongestCommonSubstringData
+    {
+        private static readonly IEnumerable<object[]> _data = new List<object[]>()
+        {
+            new object[]{ "", "", 0},
+            new object[]{ null, null, 0},
+            new object[]{ null, "", 0},
+            new object[]{ "a", "a", 1},
+            new object[]{ "abc", "cdabcf", 3},
+            new object[]{ "abc", "caba", 2},
+            new object[]{ "aaa1234567890000abcdefghigklmnopqrstuvwxyz", "aaa12345678900d00zabcdefghigklmnopqrstuvwxyz", 26},
+            new object[]{ "aaaaaaaaaa1234567890bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb", "1111111111111123456789000000000000000", 10},
+        };
+
+        public static IEnumerable<object[]> TestData => _data;
+    }
+}

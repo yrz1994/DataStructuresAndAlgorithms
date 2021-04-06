2039094e121eabafe0e85de83491478ebcc87011
Yang, Edward <yrz1994@gmail.com>
2021-04-06 11:24:54 +0800

    Add 最小成本爬楼梯

---
+++ src/LeetCode.Core/ClimbStairs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetCode.Core
+{
+    public class ClimbStairs
+    {
+        /// <summary>
+        /// 使用最小花费爬楼梯
+        /// </summary>
+        /// <param name="cost"></param>
+        /// <returns></returns>
+        public int MinCostClimbingStairs(int[] cost)
+        {
+            if (cost == null || cost.Length < 2) return 0;
+            int[] dp = new int[cost.Length + 1];
+            for(var i = 2; i <= cost.Length; i++)
+            {
+                dp[i] = Math.Min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+            }
+            return dp[cost.Length];
+        }
+    }
+}
---
+++ src/LeetCode.UnitTest/ClimbStairsTest.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode.Core;
+using System.Collections.Generic;
+using Xunit;
+
+namespace LeetCode.UnitTest
+{
+    public class ClimbStairsTest
+    {
+        [Theory]
+        [MemberData(nameof(MinCostClimbStairsData.TestData), MemberType = typeof(MinCostClimbStairsData))]
+        public void LongestCommonSubstringLengthTest(int[] cost, int expect)
+        {
+            //Arrange
+            var sut = new ClimbStairs(); //sut: system under test
+
+            //Act
+            var result = sut.MinCostClimbingStairs(cost);
+
+            //Assert
+            Assert.Equal(expect, result);
+        }
+    }
+    public class MinCostClimbStairsData
+    {
+        private static readonly IEnumerable<object[]> _data = new List<object[]>()
+        {
+            new object[]{ new int[] { 0, 0, 0, 0 }, 0 },
+            new object[]{ new int[] { 10, 15, 20 }, 15 },
+            new object[]{ new int[] { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 }, 6 }
+        };
+
+        public static IEnumerable<object[]> TestData => _data;
+    }
+}

a4a0b9ad78aa1a83800bb600cbadc6b44506eee3
Yang, Edward <yrz1994@gmail.com>
2021-04-30 17:03:13 +0800

    Add LC.4; Add LC.137;

---
+++ src/LeetCode.Core/MedianOfTwoSortedArrays.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Core
+{
+    //LC.4 寻找两个正序数组的中位数
+    public class MedianOfTwoSortedArrays
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int i = 0, j = 0;
+            var len = nums1.Length + nums2.Length;
+            var array = new int[(len / 2) + 1];
+            for (int k = 0; k < array.Length; k++)
+            {
+                if (nums2.Length == j || i < nums1.Length && nums1[i] <= nums2[j])
+                {
+                    array[k] = nums1[i];
+                    i++;
+                }
+                else
+                {
+                    array[k] = nums2[j];
+                    j++;
+                }
+            }
+            return len % 2 == 0 ? (array[array.Length - 1] + array[array.Length - 2]) / 2.0 : array[array.Length - 1];
+        }
+    }
+}
---
+++ src/LeetCode.Core/SingleNumberII.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Core
+{
+    //LC.137 只出现一次的数字 II
+    public class SingleNumberII
+    {
+        public int SingleNumber(int[] nums)
+        {
+            var dict = new Dictionary<int, int>(nums.Length);
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (dict.ContainsKey(nums[i]))
+                {
+                    dict[nums[i]] = dict[nums[i]] + 1;
+                }
+                else
+                {
+                    dict[nums[i]] = 1;
+                }
+            }
+            return dict.First(e => e.Value == 1).Key;
+        }
+
+        public int SingleNumber2(int[] nums)
+        {
+            int i = 0, j = 0;
+            while (i < nums.Length && j < nums.Length)
+            {
+                if (i != j && nums[i] == nums[j])
+                {
+                    i++;
+                    j = 0;
+                }
+                else
+                {
+                    j++;
+                }
+            }
+            return nums[i];
+        }
+    }
+}
